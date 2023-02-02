cb0af131818efdd6582e90a4664eeffcbd080e88
DevSaalih <soliualley@gmail.com>
2023-02-02 07:52:12 +0100

    update

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -1,10 +1,10 @@
 <!-- eslint-disable -->
 <template>
   <div
-    class="w-full h-full text-sm sm:text-lg inset-0 absolute bg-white overflow-hidden p-2 sm:p-6"
+    class="w-full h-full text-sm sm:text-lg inset-0 absolute bg-white overflow-auto p-2 sm:p-6"
   >
     <div
-      class="fixed m-auto inset-0 px-4 flex md:flex-row flex-col justify-center md:justify-around items-center"
+      class="m-auto inset-0 px-4 flex md:flex-row flex-col justify-center md:justify-around items-center"
     >
       <div
         class="sm:w-[300px] sm:h-[300px] lg:w-[350px] w-[150px] h-[150px] lg:h-[350px]"
@@ -42,6 +42,30 @@
           </button>
         </div>
       </div>
+    </div>
+
+    <div class="p-4 border-2 rounded-2xl mt-4 space-y-2">
+      <h1 class="underline font-medium text-center">Requirements</h1>
+      <p>1. Full Name</p>
+      <p>2. Age</p>
+      <p>3. Birth Certificate</p>
+      <p>4. Phone Number</p>
+      <p>5. Email Address</p>
+      <p>6. Nationality</p>
+      <p>7. Occupation</p>
+      <p>8. Contact Address</p>
+      <p>9. Reg. Certificate(if land usage is for Business)</p>
+      <p>10. Reg. Name</p>
+      <p>11. Reg. Number</p>
+      <p>12. Name Agent of Registration Particulars</p>
+      <p>13. Address Agent of Registration Particulars</p>
+      <p>14. Land Purpose</p>
+      <p>15. Land Size</p>
+      <p>16. Plot Number</p>
+      <p>16. Block Number</p>
+      <p>16. Street Number</p>
+      <p>17. Plot Number</p>
+      <p>18. Signature</p>
     </div>
   </div>
 </template>
